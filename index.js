8cfc1549657bea82b86b14d2f1e23369ae568447
risi <reesiomar@gmail.com>
2025-06-08 19:55:21 +0400

    chore: tested converter for functionality

---
+++ index.js
@@ -0,0 +1,17 @@
+import converter from "./converter.js";
+
+const DATA = [
+  {
+    name: 'Jason',
+    age: 15,
+    height: 166,
+  },
+  {
+    name: 'Kevin',
+    age: 19,
+    height: 186,
+  },
+]
+
+
+converter.exportFile(DATA); // test 

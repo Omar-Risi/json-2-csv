2543f8efc9876e6850592fba5fdf4a281d47a7d9
Risi <reesiomar@gmail.com>
2025-06-10 17:04:52 +0400

    chore: removed testing code

--- server/index.js
+++ server/index.js
@@ -1,17 +1,4 @@
 import converter from "./converter.js";
 
-const DATA = [
-  {
-    name: 'Jason',
-    age: 15,
-    height: 166,
-  },
-  {
-    name: 'Kevin',
-    age: 19,
-    height: 186,
-  },
-]
 
 
-converter.exportFile(DATA); // test 

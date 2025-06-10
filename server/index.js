16bfe990727da2fbe0764795baea2f5d637b0ecc
Risi <reesiomar@gmail.com>
2025-06-10 17:05:21 +0400

    feat(server): created basic server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
 import converter from "./converter.js";
+import express from 'express';
 
+const app = express();
 
+app.get('/', (req, res) => {
+  res.send("Server works");
+})
 
+app.listen(3300, () => {
+  console.log('listening on http://localhost:3300/ 🔥');
+})

b28566b687008bc7436b0e31699d163b66c9400f
risi <reesiomar@gmail.com>
2025-06-12 02:48:31 +0400

    fix(ui): replaced for with htmlFor

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -28,6 +28,7 @@ function App() {
       <main className="flex justify-center items-center flex-col h-screen">
         <div className="w-[75vw] lg:w-[50vw] lg:grid grid-cols-2 gap-4 min-h-1/2">
           <form className="flex flex-col" onSubmit={handleSubmit}>
+            <label htmlFor="code" className="font-medium">Enter JSON code</label>
             <textarea name="code" value={input} onChange={handleChange} className=" text-slate-900 flex-1 w-full border border-gray-300 rounded-md p-2 overflow-y-scroll resize-none" placeholder="Enter JSON code here"></textarea>
             <button type="submit" className="mt-4 w-full py-2 text-center px-4 bg-slate-900 text-white rounded-md font-medium uppercase hover:bg-slate-800 cursor-pointer"> Convert </button>
           </form>

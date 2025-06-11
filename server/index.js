6c854643ed26dda46f30f28604c766e0967be8f7
risi <reesiomar@gmail.com>
2025-06-12 02:48:11 +0400

    feat(ui): basic testing of client-server interaction

--- server/index.js
+++ server/index.js
@@ -3,8 +3,13 @@ const cors = require('cors');
 const converter = require('./converter.js');
 
 const app = express();
+
 app.use(express.json());
 app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('OK');
+})
 
 app.post('/api/convert', (req, res) => {
   const csvData = converter.convertData(JSON.parse(req.body)); // your conversion logic

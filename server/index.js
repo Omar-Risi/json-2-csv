f650b9db2baeb22aa8c6a7865165c3ed9beb1021
risi <reesiomar@gmail.com>
2025-06-12 02:46:06 +0400

    feat(server): Set up CORS for express

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import { convertData } from "./converter.js";
 import express from 'express';
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.post('/api/convert', (req, res) => {
   const csvData = convertData(JSON.parse(req.body)); // your conversion logic
